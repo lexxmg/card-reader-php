3e379af19e5a831d826be44bbae2c4b25e5c9c29
mac-mini <lexx.mg@gmail.com>
2022-01-22 14:10:09 +0300

    add psm object

--- js/photo-settings.js
+++ js/photo-settings.js
@@ -30,6 +30,23 @@ photoContainer.addEventListener('mouseup', event => {
   photoContainer.removeEventListener('mousemove', getPosition);
 });
 
+const psm = [  //Page segmentation modes:
+  {value: 0, text: 'Orientation and script detection (OSD) only.'},
+  {value: 1, text: 'Automatic page segmentation with OSD.'},
+  {value: 2, text: 'Automatic page segmentation, but no OSD, or OCR. (not implemented)'},
+  {value: 3, text: 'Fully automatic page segmentation, but no OSD. (Default)'},
+  {value: 4, text: 'Assume a single column of text of variable sizes.'},
+  {value: 5, text: 'Assume a single uniform block of vertically aligned text.'},
+  {value: 6, text: 'Assume a single uniform block of text.'},
+  {value: 7, text: 'Treat the image as a single text line.'},
+  {value: 8, text: 'Treat the image as a single word.'},
+  {value: 9, text: 'Treat the image as a single word in a circle.'},
+  {value: 10, text: 'Treat the image as a single character.'},
+  {value: 11, text: 'Sparse text. Find as much text as possible in no particular order.'},
+  {value: 12, text: 'Sparse text with OSD.'},
+  {value: 13, text: 'Raw line. Treat the image as a single text line, bypassing hacks that are Tesseract-specific.'}
+];
+
 function getPosition(event) {
     const cursorX = x - event.pageX;
     const cursorY = y - event.pageY;
